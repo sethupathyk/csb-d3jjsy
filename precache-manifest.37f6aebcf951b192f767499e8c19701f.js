b3fbd796df0c8cc27b4c2a1d2a7f288df2766fa0
Code Sandbox <hello@codesandbox.io>
2022-05-09 06:43:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.37f6aebcf951b192f767499e8c19701f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37fb07808923fd15d744",
+    "url": "https://sethupathyk.github.io/csb-d3jjsy/static/css/main.dda56c0a.chunk.css"
+  },
+  {
+    "revision": "37fb07808923fd15d744",
+    "url": "https://sethupathyk.github.io/csb-d3jjsy/static/js/main.ba251cd2.chunk.js"
+  },
+  {
+    "revision": "ccbc148926ffdaf8d336",
+    "url": "https://sethupathyk.github.io/csb-d3jjsy/static/js/runtime~main.0be7bab2.js"
+  },
+  {
+    "revision": "9ab08409fa2329d1c942",
+    "url": "https://sethupathyk.github.io/csb-d3jjsy/static/js/2.becacbb4.chunk.js"
+  },
+  {
+    "revision": "6c4cc9e172df6645ba26d52b11d669c6",
+    "url": "https://sethupathyk.github.io/csb-d3jjsy/index.html"
+  }
+];
